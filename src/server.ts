6f5904a458f8d7ab3793241c8e3264faaf0835e1
Goncalo Silva <goncalofmsilva2004@gmail.com>
2026-03-30 19:02:38 +0100

    app and server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express from "express"
+import app from './app.js'
 import dotenv from "dotenv"
-const app = express()
+
 dotenv.config()
-const PORT = process.env.PORT
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
